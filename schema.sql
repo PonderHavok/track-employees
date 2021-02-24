aa5bf7ba2b8c907a59908cf83c9afb4b745d4618
Jonny Talbott <transverseponderer@gmail.com>
2021-02-23 23:52:01 -0500

    Uploading code

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS emptracker_db;
+CREATE DATABASE emptracker_db;
+USE emptracker_db;
+
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY(role_id) REFERENCES role(id),
+  FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  department_id INT,
+  salary DECIMAL,
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO
+  department (name)
+VALUES
+  ("Human Resources"),
+  ("Safety"),
+  ("Engineering"),
+  ("Accounting"),
+  ("Manufacturing");
+INSERT INTO
+  role (title, salary, department_id) VALUE ("manager", 78000.00, 2),
+  ("engineer", 85000, 1),
+  ("human resource rep", 55000, 2),
+  ("accountant", 65080, 3),
+  ("security", 40000, 4),
+  ("manual labor", 48750, 5);
+INSERT INTO
+  employee (first_name, last_name, role_id) VALUE ("Joy", "Ryder", 2),
+  ("Justin", "Ensane", 2),
+  ("Anita", "Goodman", 3),
+  ("Cara", "Van", 5),
+  ("Helen", "Back", 4),
+  ("Jean", "Poole", 1),
+  ("Daryl", "Rhea", 1);
+UPDATE
+  `emptracker_db`.`employee`
+SET
+  `manager_id` = '1'
+WHERE
+  (`id` > '1');
